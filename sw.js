067a716a052e79da082fd373ce34d30436cdc053
O'Shaughnessy, Peter <Peter.OShaughnessy@pearson.com>
2016-08-15 20:11:51 +0100

    Add another couple of examples for sanity

--- sw.js
+++ sw.js
@@ -1,8 +1,21 @@
 self.addEventListener('fetch', event => {
 
   if (event.request.url.endsWith('/test')) {
-    event.respondWith(new Response('This is the text that should be downloaded',
-      {headers: { 'Content-Type': 'text/plain' }}));
+
+    event.respondWith
+      (new Response('This is the text that should be downloaded'),
+        {headers: {'Content-Type': 'text/plain'}});
+
+    // (Also tried without the header)
+
+  } else if (event.request.url.endsWith('/test-html')) {
+
+    // Example from: https://jakearchibald.com/2015/thats-so-fetch/
+    event.respondWith(
+      new Response('<h1>Hello!</h1>',
+        {headers: {'Content-Type': 'text/html'}})
+    );
+
   }
 
 });
